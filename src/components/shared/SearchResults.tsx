babf0e05a94fa6b3aaf0173ff97557aa0fc8adee
Divyesh Patel <pdev89348@gmail.com>
2024-10-18 15:00:01 +0530

    first commit

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import { Models } from 'appwrite'
+import Loader from './Loader'
+import GridPostList from './GridPostList'
+
+type SearchResultsProps = {
+  isSearchFetching: boolean;
+  searchedPosts: any;
+}
+
+const SearchResults = ({
+  isSearchFetching,
+  searchedPosts,
+}: SearchResultsProps) => {
+  if (isSearchFetching) return <Loader />
+  console.log(searchedPosts)
+
+  if (searchedPosts && searchedPosts.length > 0) 
+    return (
+      <GridPostList posts={searchedPosts} />
+    )
+  return (
+    <p className='text-light-4 mt-10 text-center w-full'>No results found</p>
+  )
+  
+}
+
+
+export default SearchResults
